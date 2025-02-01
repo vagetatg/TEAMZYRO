1e92edf581b53357eba1a59a39bfa30aa3e24760
vagetatg <nibbanmisal3302@gmail.com>
2025-02-01 19:59:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,6 +51,7 @@ GIT_TOKEN = getenv(
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PiratesBotRepo")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PiratesMainchat")
+UPDATE_CHAT = getenv("UPDATE_CHAT", "https://t.me/PiratesMainchat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
