88ca9f6e9b0bdbbbe2e8960e6552b744b0a860ab
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-01 15:00:48 +0530

    Update __init__.py

--- TEAMZYRO/__init__.py
+++ TEAMZYRO/__init__.py
@@ -5,18 +5,19 @@ from TEAMZYRO.core.dir import dirr
 from TEAMZYRO.core.git import git
 from TEAMZYRO.core.userbot import Userbot
 from TEAMZYRO.misc import dbb, heroku
-
+from TEAMZYRO.core.application import main, application
 from .logging import LOGGER
 
 dirr()
 git()
 dbb()
 heroku()
+main()
 
 app = Hotty()
 userbot = Userbot()
 api = SafoneAPI()
-
+apl = application
 
 from .platforms import *
 
